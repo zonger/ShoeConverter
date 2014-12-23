c9865eaca85929f78f4771bdcdfbdb0808e3670e
ryanpixilink <ryan@pixilink.com>
2014-12-23 12:17:58 -0800

    Initial Commit

---
+++ ShoeConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeConverter
+//
+//  Created by Ryan Liu on 2014-12-23.
+//  Copyright (c) 2014 Ryan Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
